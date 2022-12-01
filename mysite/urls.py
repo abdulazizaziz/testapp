46e56153fd9e67432e50c93d0f8999826fbd5e50
abdulazizaziz <75569778+abdulazizaziz@users.noreply.github.com>
2022-12-01 10:53:20 +0500

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.http.response import HttpResponse
+
+def home(request):
+    return HttpResponse('<h1>Hello, World</h1>')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
